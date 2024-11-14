68ab9eb87ecdb7783adf2893c7dd14f687c4ebb5
Kelsey Kuno <kelsey.kuno@gmail.com>
2024-11-14 09:37:34 -0500

    revert commit

--- content/queries.ts
+++ content/queries.ts
@@ -1,39 +1,6 @@
 import { HeroQuery } from "@/types"
 import { contentGQLQuery } from "./fetch"
 
-export const getContentForBlogPost = async (slug:string) => {
-  const query = `#graphql
-  query BlogPostCollection($where: BlogPostFilter) {
-  blogPostCollection(where: $where) {
-    items {
-      body {
-        json
-      }
-      _id
-      ctaCollection {
-        items {
-          href
-          text
-        }
-      }
-      slug
-      summary
-      title
-    }
-  }
-}
-`
-const data = await contentGQLQuery({query, variables: {
-  where: {
-    slug,
-  }
-}})
-if (!data) {
-  throw Error("oops no blogs")
-}
-return data
-}
-
 export const getContentForHero = async () => {
     const query = `#graphql
     query HeroCollection {
--- types.ts
+++ types.ts
@@ -1,31 +1,12 @@
-export type BlogPostQuery = {
-
-  blogPostCollection: {
-    items: {
-      body: {
-        json: JSON
-      }
-      _id: string
-      ctaCollection: {
-        items: {
-          href: string
-          text: string
-        }
-      }[]
-      slug: string
-      summary:string
-      title:string
-    }[]
-  }
-}
 
 
-export type HeroQuery = {
-    heroCollection {
-        items: {
-          title: string
-          preTitle: string
-        }[]
-      }
-    }
-}
+
+// export type HeroQuery = {
+//     heroCollection {
+//         items: {
+//           title: string
+//           preTitle: string
+//         }[]
+//       }
+//     }
+// }

97d9831b3a0374585f5e0d3745f06b665514246f
Kelsey Kuno <kelsey.kuno@gmail.com>
2024-11-21 14:04:47 -0500

    css style tests

--- app/blog/(posts)/[slug]/page.tsx
+++ app/blog/(posts)/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getContentForBlogPost, getAllBlogSlugs } from "@/content/queries";
 import Image from "next/image";
+import Styles from "./page.module.css";
 
 export const generateStaticParams = async () => {
   const slugData = await getAllBlogSlugs();
@@ -12,15 +13,21 @@ export default async function BlogPostPage({ params }) {
   const coverImage = content.imageCollection.items[0];
 
   return (
-    <div>
-      <h2>dynamic blog post data</h2>
-      <p>hello fetch blog post data: {content.title}</p>
-      <Image
-        src={coverImage.url}
-        alt={coverImage.description}
-        width={coverImage.width}
-        height={coverImage.height}
-      />
+    <div className="container">
+      <div className={`flex ${Styles.paper}`}>
+        <div>
+          <h2>{content.title}</h2>
+          <h4>{content.summary}</h4>
+        </div>
+        <div>
+          <Image
+            src={coverImage.url}
+            alt={coverImage.description}
+            width={coverImage.width}
+            height={coverImage.height}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,24 @@
+import styles from "./page.module.css";
+
 console.log("hello");
 
 const ProjectPage = () => {
-  return <div>Projects yo</div>;
+  return (
+    <div className="container">
+      <div className="flex">
+        <h6>Projects yooo</h6>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</h2>
+      </div>
+    </div>
+  );
 };
 export default ProjectPage;

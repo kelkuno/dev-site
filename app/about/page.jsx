b6c56d880d93cf4a75bc44a65049be39e159fc39
Kelsey Kuno <kelsey.kuno@gmail.com>
2024-12-17 10:54:33 -0500

    added about page base styles

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,47 +1,92 @@
+import styles from "./about.module.css";
+import Image from "next/image";
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>This is Heading 1</h1>
-      <h2>This is Heading 2</h2>
-      <h3>This is Heading 3</h3>
-      <h4>This is Heading 4</h4>
-      <h5>This is Heading 5</h5>
-      <p className="date">This is date</p>
-      <p className="sml-title">This is small title</p>
-      <div>
-        <a className="primary-link" href="#">
-          Learn more
-        </a>
-      </div>
-      <button className="primary-btn">Download</button>
-      <p>
-        This is a paragraph. Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Quisquam, pariatur.
-      </p>
-      <a href="#">A text link by itself</a>
-      <ul>
-        <li>This is an unordered list.</li>
-        <li>Second thing.</li>
-        <li>Third thing.</li>
-      </ul>
-      <strong>Strongly emphasized text</strong>
-      <ol>
-        <li>This is an ordered list.</li>
-        <li>Second thing.</li>
-        <li>Third thing.</li>
-      </ol>
-      <em>Emphasized text</em>
-      <blockquote>
-        <p>A paragraph inside of a blockquote, said by a famous person.</p>
-      </blockquote>
-
-      <figure>
-        <img
-          src="https://assets.codepen.io/296057/fem-blindfold-475.jpg"
-          alt="The three men of Magenta Lime, blindfolded."
-        />
-        <figcaption>Magenta Lime will rock your world.</figcaption>
-      </figure>
+    <div className={styles["about-wrapper"]}>
+      <section className="container section-gap">
+        <div className={styles["hero-container"]}>
+          <div className={styles.hero__title}>
+            <h1>Here is something about me.</h1>
+          </div>
+          <div className={styles["hero-img-wrapper"]}>
+            <Image
+              src="/images/placeholder.png"
+              alt="Handwritten note saying, Hello. My name is Kelsey"
+              className={styles["hero__img"]}
+              width={800}
+              height={200}
+              priority
+            />
+            <div className={styles["hero__link"]}>
+              <a className="primary-link" href="#">
+                Read More
+              </a>
+            </div>
+          </div>
+          <div className={`${styles["about-grid"]} grid`}></div>
+        </div>
+      </section>
+      <section className="container">
+        <div className="row">
+          <div className={styles["about-content"]}>
+            <h5>
+              My career journey has been all over the place but at the same
+              time, right on time.
+            </h5>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Aenean ligula
+              aliquam tincidunt nulla convallis mi nisi nunc. Phasellus lobortis
+              egestas suscipit lacus lectus fusce dictum. Velit malesuada
+              viverra nostra at rhoncus suscipit in. Mollis arcu elit mus,
+              vehicula neque blandit neque? Sodales blandit leo viverra taciti
+              mus. Pharetra finibus vehicula interdum feugiat vestibulum.
+              Sodales mattis natoque etiam potenti amet nec quam.
+            </p>
+            <p>
+              Metus quisque non consequat aliquam placerat. Mattis risus
+              facilisis aliquet lorem turpis hendrerit. Enim morbi etiam; odio
+              viverra rhoncus bibendum nulla. Condimentum rutrum nam non laoreet
+              magnis elementum; molestie ac? Montes magnis enim massa;
+              scelerisque suscipit congue pretium. Class volutpat luctus
+              penatibus magnis dignissim habitant ad curabitur. Risus quam
+              taciti inceptos quis vehicula ullamcorper.
+            </p>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Aenean ligula
+              aliquam tincidunt nulla convallis mi nisi nunc. Phasellus lobortis
+              egestas suscipit lacus lectus fusce dictum. Velit malesuada
+              viverra nostra at rhoncus suscipit in. Mollis arcu elit mus,
+              vehicula neque blandit neque? Sodales blandit leo viverra taciti
+              mus. Pharetra finibus vehicula interdum feugiat vestibulum.
+              Sodales mattis natoque etiam potenti amet nec quam.
+            </p>
+            <p>
+              Metus quisque non consequat aliquam placerat. Mattis risus
+              facilisis aliquet lorem turpis hendrerit. Enim morbi etiam; odio
+              viverra rhoncus bibendum nulla. Condimentum rutrum nam non laoreet
+              magnis elementum; molestie ac? Montes magnis enim massa;
+              scelerisque suscipit congue pretium. Class volutpat luctus
+              penatibus magnis dignissim habitant ad curabitur. Risus quam
+              taciti inceptos quis vehicula ullamcorper.
+            </p>
+          </div>
+          <div className={styles["about-skills"]}>
+            <ul>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+              <li>UX Design</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };

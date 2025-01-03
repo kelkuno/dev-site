77aa1e1cde8e3b36753229134d64d1f52b0ae3aa
Kelsey Kuno <kelsey.kuno@gmail.com>
2025-01-03 12:36:23 -0500

    added see CV functionality

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,7 +16,14 @@ export default function Home() {
             priority
           />
           <h2>I am a creative developer amet, consectetuer adipiscing elit.</h2>
-          <button className="primary-btn">Download CV</button>
+          <Link
+            href="/kelsey-kuno-resume-2024.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="primary-btn"
+          >
+            See CV
+          </Link>
         </div>
         <h1>Creative Developer</h1>
       </section>

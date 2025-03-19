29bfe67f82cb064760bb3f5659f6cfa7a8183abb
Kelsey Kuno <kelsey.kuno@gmail.com>
2025-03-19 15:06:45 -0400

    updated home page component with collage of work cover

--- app/page.js
+++ app/page.js
@@ -2,15 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import FooterNav from "@/components/FooterNav/FooterNav";
-import SkillsList from "@/components/SkillsList/SkillsList";
 
 export default function Home() {
   return (
     <main className="container">
       <section>
+        <div className={styles["home-content"]}>
+          <h1>Creative Technologist</h1>
+        </div>
         <div className={styles["drawing-wrapper"]}>
           <Image
-            src="/images/kk-profile-drawing.png"
+            src="/images/work-collage-writing.jpg"
             alt="A digital drawing of Kelsey Kuno"
             width={6}
             height={4}
@@ -18,12 +20,6 @@ export default function Home() {
             priority
             layout="responsive"
           />
-        </div>
-        <div className={styles["home-content"]}>
-          <h1>
-            Creative<br></br> Technologist
-          </h1>
-          <SkillsList />
         </div>
       </section>
     </main>
